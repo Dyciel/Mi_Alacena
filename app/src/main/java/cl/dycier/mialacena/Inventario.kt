1f41ac01d5c2da19441b79723348ce69bc6198c7
Diego <dcastro20@alumnos.utalca.cl>
2023-10-05 01:29:54 -0300

    Inserción de posibles pantallas

---
+++ app/src/main/java/cl/dycier/mialacena/Inventario.kt
@@ -0,0 +1,11 @@
+package cl.dycier.mialacena
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inventario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inventario)
+    }
+}
